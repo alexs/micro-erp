fb988758d39f3f932769dac8654867ff1887905f
Alex <alexs1010@gmail.com>
2013-11-05 16:50:36 -0600

    Starting webapp with .gitignore and basics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erp::Application.config.secret_token = 'd93dcacf53f260dda75469db2dedace775df2fdb844326623f0793bb06d5c76288b47fe0910f9294676d1ee29b47e020506268f82855cb0c2c0c84dc2fe232c1'
